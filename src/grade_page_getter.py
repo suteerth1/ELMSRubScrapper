e475e9ee20ea4d93e8c01185b16f9dc995c43041
suteerth <vishnusuteerth@gmail.com>
2018-01-20 02:29:39 -0500

    Init commit

---
+++ src/grade_page_getter.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import re
+import os
+import pycurl
+
+GRADES_TXT = "../data/ids.txt"
+TOK_FILE = "../data/auth_tok.txt"
+COOKIES_TXT = "../data/cookies.txt"
+OUTPUT_FOLDER = "../data/grades"
+COURSE_NUMBER = "<COURSE_NUMBER>"
+COURSE_ID = "<COURSE_ID>"
+
+def get_student_ids(access_token_filename, course_number):
+    tok = open(access_token_filename).read().strip()
+    url = "https://myelms.umd.edu/api/v1/courses/sis_course_id:" + course_number + "/enrollments"  # + str(course_number) +"/enrollments"
+    regex_page_number = re.compile("[^_]page=(\d+)")
+    req = requests.get(url, data={"access_token": tok, "per_page": 100})
+    req_links = req.headers['Link']
+    req_links_arr = req_links.split(",")
+    try:
+        number_of_pages = int(regex_page_number.findall(req_links_arr[3])[0])
+    except IndexError:
+        print("Cant find the numbe of pages because the last page was not found in headers")
+    print(number_of_pages)
+    # ids = json.loads(temp.text)
+    ret = []
+    for i in range(1, number_of_pages):
+        req = requests.get(url, data={"access_token": tok, "per_page": 100, "page": i})
+        student_arr = json.loads(req.text)
+        for stud in student_arr:
+            if stud["type"] == "StudentEnrollment":
+                new_html_url = stud["html_url"]
+                new_html_url = re.compile("/users/").sub("/grades/", stud["html_url"])
+
+                ret.append(new_html_url)
+    return ret
+
+
+def get_student_pages(cookies_txt, ids, course_id):
+    with open(ids, "r") as stud_ids:
+        for line in stud_ids:
+            print("[-] getting "+repr(line))
+            stud_id = line.split('/')[-1].strip()
+            os.system('wget  -O '+OUTPUT_FOLDER+'/'+stud_id+' --load-cookies '+cookies_txt+' "https://umd.instructure.com/courses/'+course_id+'/grades/'+stud_id+'"')
+            print(line)
+
+            print("[+] Written to " + stud_id)
+
+
+
+if __name__ == "__main__":
+    temp = get_student_ids(TOK_FILE, COURSE_NUMBER)
+    print(temp)
+    with open(GRADES_TXT, "w") as putemhere:
+        for i in temp:
+            putemhere.write(i + "\n")
+
+    print("getting student pages")
+    get_student_pages(COOKIES_TXT, GRADES_TXT, COURSE_ID)
+# print(temp.headers)
---
+++ src/html_parser.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import re
+import csv
+
+
+SUBID_RE = re.compile("submission_(\d+)")
+DIR = "../data/grades"
+OUTPUT = "../data/scores.csv"
+FIELD_NAMES = "../data/field_name.txt"
+
+
+def parse_html(html):
+    sub_ids = SUBID_RE.findall(html)
+    soup = BeautifulSoup(html, "lxml")
+    ret = dict()
+    name = re.compile("Grades for (.*): C").findall(html)[0]
+    ret["s_name"] = name
+    for h in soup.find_all(id=["submission_" + i for i in sub_ids]):
+        _id = h["id"].split("_")[1]
+        for r in soup.find_all(id="rubric_" + _id):
+            trs = r.find_all(id=re.compile("criterion__\d+"))
+            for tr in trs:
+                lab = tr.find(attrs={"class": "criterion_description_value"})
+                if lab:
+                    lab = lab.string
+                    score = list(tr.find("td", class_="nobr points_form").stripped_strings)
+                    score = (score[1])  # + "/" + score[3])
+                    assn_name = list(h.stripped_strings)[0]
+                    lab_bits = lab.split(" ")
+                    ret[assn_name[0] + assn_name[-1] + lab_bits[0][0] + lab_bits[1]] = score
+    return ret
+
+
+def process_html(inf, dwriter, keys):
+    scores = parse_html(inf.read())
+    dwriter.writerow(scores)
+
+
+if __name__ == "__main__":
+    with open(DIR + "/../ids.txt") as lst:
+        outf = open(OUTPUT, 'w')
+        field_name = [x.strip() for x in open(FIELD_NAMES, 'r').read().split(',')]
+        print(field_name)
+        dwriter = csv.DictWriter(outf, fieldnames=field_name)
+
+        dwriter.writeheader()
+        for html in lst:
+            html = html.split("/")[-1]
+            id_ = html
+            try:
+                inf = open(DIR + "/" + html.strip())
+                html = inf.read()
+                inf.close()
+                row = parse_html(html)
+                row["s_id"] = id_
+                for k in field_name:
+                    row[k] = row.get(k, '')
+                print("[+] processed " + row["s_name"] + " id:" + row["s_id"])
+                (dwriter.writerow(row))
+            except FileNotFoundError:
+                print("[x] File not found")
+
+
+        outf.close()
